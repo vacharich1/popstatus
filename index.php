9983ce0cccea9b909e2647a38674600961be4ace
jay <jr30149_@hotmail.com>
2017-09-02 23:33:18 +0700

    sdfdfsdfds

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (!is_null($events['events'])) {
 		//1: pop jay test C90392ece5d6b6e69b8a2619e36e018f6
 		//1: pop status C7eb1d9d359ca234aa91de883c1fe2a4a
 		//2: pop room3 Cd79d1bc113f459056f31193db6b5b321
-		if($event['source']['groupId'] == 'C90392ece5d6b6e69b8a2619e36e018f6' || $event['source']['groupId'] == 'C7eb1d9d359ca234aa91de883c1fe2a4a' || $event['source']['groupId'] == 'Cebf06fc279e81c0f80518bbcd5c03067' || $event['source']['groupId'] == 'Cb07e6ef503794c6e9a7fb0fedb324b07' || $event['source']['groupId'] == 'Cd79d1bc113f459056f31193db6b5b321')
+		if($event['source']['groupId'] == 'Cb6942ed51d87f590d8c289478f4b517d' || $event['source']['groupId'] == 'C90392ece5d6b6e69b8a2619e36e018f6' || $event['source']['groupId'] == 'C7eb1d9d359ca234aa91de883c1fe2a4a' || $event['source']['groupId'] == 'Cebf06fc279e81c0f80518bbcd5c03067' || $event['source']['groupId'] == 'Cb07e6ef503794c6e9a7fb0fedb324b07' || $event['source']['groupId'] == 'Cd79d1bc113f459056f31193db6b5b321')
 		{
 			// Reply only when message sent is in 'text' format
 				if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
@@ -53,7 +53,7 @@ if (!is_null($events['events'])) {
 								$hoonname = substr($text, 1); // cut@
 								if($event['source']['userId'] == 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b')
 								{
-									$room='Cd79d1bc113f459056f31193db6b5b321';
+									$room='Cb6942ed51d87f590d8c289478f4b517d';
 								}
 								else
 								{
